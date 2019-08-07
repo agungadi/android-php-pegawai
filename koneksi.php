e64ebbd5e2f5e61164660e00aa8023c488eaac1b
agungadi <agungadihack@gmail.com>
2019-08-07 21:22:14 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,20 +1,10 @@
 <?php
  //Mendefinisikan Konstanta
- define('HOST','localhost');
- define('USER','root');
- define('PASS','');
+ define('HOST','ec2-174-129-226-232.compute-1.amazonaws.com');
+ define('USER','yqkincusuhqrzt');
+ define('PASS','938dd304476a16ccb219d0432f20c45a22ff4944b77783fd3429aef06f46dfe5');
+ define('PORT','5432');
  define('DB','db_android');
 
-   $con = "dbname=dei1dugtm16cah host=ec2-174-129-226-232.compute-1.amazonaws.com port=5432 user=yqkincusuhqrzt password=938dd304476a16ccb219d0432f20c45a22ff4944b77783fd3429aef06f46dfe5 sslmode=require";
-
-
-   if (!$con) 
-   {
-     echo "Database connection failed.";
-   }
-   else 
-   {
-     echo "Database connection success.";
-   }
-
+ $con = mysqli_connect(HOST,USER,PASS,PORT,DB) or die('Unable to Connect');
 ?>
